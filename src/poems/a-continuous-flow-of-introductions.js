e396d73744be4ab3ed17751ae1b8e65a396cc0aa
JAN <janusbased@gmail.com>
2019-11-08 19:40:10 -0600

    Space fix.

--- src/poems/a-continuous-flow-of-introductions.js
+++ src/poems/a-continuous-flow-of-introductions.js
@@ -6,50 +6,50 @@ const AContinuousFlowOfIntroductions = ({ cursorVisibility }) => {
     <Link
       id={cursorVisibility}
       className={`a-continuous-flow-of-introductions link none`}
-      to="/"
+      to='/'
     >
-      <div className="vertically-centered">
-        <div className="stanza">
-          <p className="quarter">a continuous flow of introductions—</p>
-          <p className="half">the intoning children of the evicted</p>
-          <p className="three-quarters">—fellow-feeling—gave imitations</p>
+      <div className='vertically-centered'>
+        <div className='stanza'>
+          <p className='quarter'>a continuous flow of introductions—</p>
+          <p className='half'>the intoning children of the evicted</p>
+          <p className='three-quarters'>—fellow-feeling—gave imitations</p>
           <p>of the bruising origin—</p>
         </div>
-        <div className="stanza top-margin">
-          <p className="quarter">its bare possibility—I became fief-holder</p>
-          <p className="half">when I loved —smiley to know the taste</p>
-          <p className="three-quarters">
+        <div className='stanza top-margin'>
+          <p className='quarter'>its bare possibility—I became fief-holder</p>
+          <p className='half'>when I loved—smiley to know the taste</p>
+          <p className='three-quarters'>
             of it—and then put it off—for a little
           </p>
           <p>while—</p>
         </div>
-        <div className="stanza top-margin">
-          <p className="quarter">at the social function—the deathbed—</p>
-          <p className="half">a convulsive start made us a hereafter—</p>
-          <p className="three-quarters">
+        <div className='stanza top-margin'>
+          <p className='quarter'>at the social function—the deathbed—</p>
+          <p className='half'>a convulsive start made us a hereafter—</p>
+          <p className='three-quarters'>
             that unattainable there—sets blinking
           </p>
           <p>eyes already accustomed—</p>
         </div>
-        <div className="stanza top-margin">
-          <p className="quarter">to not seeing</p>
-          <p className="half">first farewells</p>
-          <p className="three-quarters">lengthen fetters . . .</p>
+        <div className='stanza top-margin'>
+          <p className='quarter'>to not seeing</p>
+          <p className='half'>first farewells</p>
+          <p className='three-quarters'>lengthen fetters . . .</p>
           <p>words effuse—</p>
         </div>
-        <div className="stanza top-margin">
-          <p className="quarter">
+        <div className='stanza top-margin'>
+          <p className='quarter'>
             Well, well, there are no cliffs here, but you don’t mind if I sit
             down beside you, all the same,
           </p>
-          <p className="half">
+          <p className='half'>
             as I used to? Let’s just sit and talk quietly, so that displeasure
             should not be taken seriously.
           </p>
         </div>
-        <div className="stanza top-margin">
-          <p className="three-quarters">But now-insipid others wait for us,</p>
-          <p className="one">and we’ll do it for free.</p>
+        <div className='stanza top-margin'>
+          <p className='three-quarters'>But now-insipid others wait for us,</p>
+          <p className='one'>and we’ll do it for free.</p>
         </div>
       </div>
     </Link>
