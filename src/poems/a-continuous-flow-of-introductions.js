192edaa3308bff04ca5a8acd555c48b95fc1ca26
JAN <janusbased@gmail.com>
2019-11-08 20:05:40 -0600

    New version of A continous flow of introductions.

--- src/poems/a-continuous-flow-of-introductions.js
+++ src/poems/a-continuous-flow-of-introductions.js
@@ -21,15 +21,7 @@ const AContinuousFlowOfIntroductions = ({ cursorVisibility }) => {
           <p className='three-quarters'>
             of it—and then put it off—for a little
           </p>
-          <p>while—</p>
-        </div>
-        <div className='stanza top-margin'>
-          <p className='quarter'>at the social function—the deathbed—</p>
-          <p className='half'>a convulsive start made us a hereafter—</p>
-          <p className='three-quarters'>
-            that unattainable there—sets blinking
-          </p>
-          <p>eyes already accustomed—</p>
+          <p>while eyes—already accustomed</p>
         </div>
         <div className='stanza top-margin'>
           <p className='quarter'>to not seeing</p>
@@ -46,10 +38,6 @@ const AContinuousFlowOfIntroductions = ({ cursorVisibility }) => {
             as I used to? Let’s just sit and talk quietly, so that displeasure
             should not be taken seriously.
           </p>
-        </div>
-        <div className='stanza top-margin'>
-          <p className='three-quarters'>But now-insipid others wait for us,</p>
-          <p className='one'>and we’ll do it for free.</p>
         </div>
       </div>
     </Link>
