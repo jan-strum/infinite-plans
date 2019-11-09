35304f3de2e51d9121a8e45c04424f540bc5bd5e
JAN <janusbased@gmail.com>
2019-11-08 20:27:15 -0600

    New version of Regent of Interregnum.

--- src/poems/Regent-of-Interregnum.js
+++ src/poems/Regent-of-Interregnum.js
@@ -5,18 +5,18 @@ const RegentOfInterregnum = ({ cursorVisibility }) => {
   return (
     <Link
       id={cursorVisibility}
-      className="Regent-of-Interregnum link none"
-      to="/"
+      className='Regent-of-Interregnum link none'
+      to='/'
     >
       <p>Regent of Interregnum addresses samely disparate hands.</p>
       <p>They point to supermarkets full of fruit and nuts</p>
       <p>and glass cases labeled Positive Evidence,</p>
       <p>reporting me and my cause to those amassing human units.</p>
       <br />
-      <p>Silence sits, its hands hanging down,</p>
-      <p>dripping on an altar that founds its own invisibility.</p>
-      <p>Its song gone dry speaks to its armor.</p>
-      <p>Its slow thighs almost dance.</p>
+      <p>He sits, his hands hanging down,</p>
+      <p>dripping on an altar that founds my invisibility.</p>
+      <p>His song, gone dry, speaks to his armor.</p>
+      <p>His slow thighs almost dance.</p>
       <br />
       <p>An extravagant crowning excuse besieges little life.</p>
     </Link>
