807e0e759766f5f2e7dd33ef7dfcf40f46dfab5f
JAN <janusbased@gmail.com>
2019-11-14 07:54:02 -0600

    New version of The crowd presses me.

--- src/poems/The-crowd-presses-me.js
+++ src/poems/The-crowd-presses-me.js
@@ -5,17 +5,17 @@ const TheCrowdPressesMe = ({ cursorVisibility }) => {
   return (
     <Link
       id={cursorVisibility}
-      className="The-crowd-presses-me link none"
-      to="/"
+      className='The-crowd-presses-me link none'
+      to='/'
     >
-      <div className="vertically-centered">
+      <div className='vertically-centered'>
         <p>The crowd presses me toward big</p>
         <p> doors in constant action. They</p>
         <p>open and close the interim called:</p>
         <p>“We shall look at it tomorrow.”</p>
         <p>I, chattering animal, had to show</p>
         <p>my teeth and go on, as though</p>
-        <p>speaking of a song transposed</p>
+        <p>thinking of a song transposed</p>
         <p>in sleep an octave higher.</p>
       </div>
     </Link>
