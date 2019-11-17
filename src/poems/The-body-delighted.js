a295d425858c607f4c3a6db0d22815512a76a2e5
JAN <janusbased@gmail.com>
2019-11-16 20:44:10 -0600

    WIP: New version of The body, delighted.

--- src/poems/The-body-delighted.js
+++ src/poems/The-body-delighted.js
@@ -3,38 +3,33 @@ import { Link } from 'react-router-dom'
 
 const TheBodyDelighted = ({ cursorVisibility }) => {
   return (
-    <Link id={cursorVisibility} className="The-body-delighted link none" to="">
+    <Link id={cursorVisibility} className='The-body-delighted link none' to=''>
       <p>The body, delighted by being unlocatable, exhaling</p>
-      <p className="half">
+      <p className='half'>
         {' '}
         from behind the flow of a veil, acquired for itself
       </p>
-      <p className="one">
+      <p className='half'>
         the virginity of a foreign site displayed under a fermata.
       </p>
       <br />
-      <p>Guts, no sooner scattered, spring forth incidentally sovereign</p>
-      <p className="half">—blown from where?</p>
-      <br />
-      <p>Retreats of a skirt and herself instituting a place.</p>
-      <p className="half">All distractions from life pulled in, communicated</p>
-      <p className="one">
+      <p>Retreats of a skirt and herself-instituting a place.</p>
+      <p className='half'>All distractions from life pulled in, communicated</p>
+      <p className='half'>
         to impersonal, eurythmic surfaces of steamy semi-nudes.
       </p>
       <br />
-      <p>
-        Chastity is a qualifier—being split open, relieved of the weight of its
-        flesh.
-      </p>
+      <p>Guts, no sooner scattered, spring forth</p>
+      <p className='half'>—blown from where?</p>
       <br />
-      <p>
-        The formula of self-coronation masks the marginal exorbitance of a
-        relational act.
-      </p>
+      <p>Chastity is a qualifier—being split open—</p>
+      <p className='half'>relieved of the weight of its flesh.</p>
       <br />
-      <p>
-        Myth-sized life folds into doctrine, laid on the bed of sebum silks.
-      </p>
+      <p>The formula of self-coronation masks</p>
+      <p className='half'>the marginal exorbitance of a relational act.</p>
+      <br />
+      <p>Myth-sized life folds into doctrine,</p>
+      <p className='half'>laid on the bed of sebum silks.</p>
     </Link>
   )
 }
