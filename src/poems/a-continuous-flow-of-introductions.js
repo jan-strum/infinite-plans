20e6d2bab53ff9217490b7e9f8c2081f98a2a944
JAN <janusbased@gmail.com>
2020-01-18 11:26:03 -0600

    A continuous flow of introductions: new version.

--- src/poems/a-continuous-flow-of-introductions.js
+++ src/poems/a-continuous-flow-of-introductions.js
@@ -21,13 +21,21 @@ const AContinuousFlowOfIntroductions = ({ cursorVisibility }) => {
           <p className='three-quarters'>
             of it—and then put it off—for a little
           </p>
-          <p>while eyes—already accustomed</p>
+          <p>while—</p>
+        </div>
+        <div className='stanza top-margin'>
+          <p className='quarter'>at the social function—a small committee</p>
+          <p className='half'>made us a hereafter—that happy, belated,</p>
+          <p className='three-quarters'>
+            unattainable there—and set bedroom eyes
+          </p>
+          <p>already accustomed</p>
         </div>
         <div className='stanza top-margin'>
           <p className='quarter'>to not seeing</p>
           <p className='half'>first farewells</p>
           <p className='three-quarters'>lengthen fetters . . .</p>
-          <p>words effuse—</p>
+          <p>words effuse:</p>
         </div>
         <div className='stanza top-margin'>
           <p className='quarter'>
